8faef4a68cef2c72b6d9cd9d8a17002ff8693b8f
muffinfeng <1697426994@qq.com>
2021-05-24 23:17:00 +0800

    no message

--- src/pwa-learn/sw.js
+++ src/pwa-learn/sw.js
@@ -1,12 +1,38 @@
+const CACHE_NAME = 'cache-v6';
 self.addEventListener('install', event => {
     console.log('install', event);
-    event.waitUntil(self.skipWaiting());
-    //gfdg
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => {
+        cache.addAll([
+            '/',
+            './index.css'
+        ]);
+    }));
+    
 });
 self.addEventListener('activate', event => {
     console.log('activate',event);
-    event.waitUntil(self.ClientRectList.claim());
+    event.waitUntil(caches.keys().then(cacheNames => {
+        return Promise.all(cacheNames.map(cacheName => {
+            if(CACHE_NAME !== cacheName){
+                return caches.delete(cacheName);
+            }
+        }))
+    }))
+   
 });
 self.addEventListener('fetch', event => {
     console.log('fetch',event);
+    event.respondWith(caches.open(CACHE_NAME).then(cache => {
+        return cache.match(event.request).then(response => {
+            if(response){
+                return response;
+            }
+
+            return fetch(event.request).then(response => {
+                cache.put(event.request, response.clone());
+                return response;
+            });
+        })
+    }));
 });
+//
